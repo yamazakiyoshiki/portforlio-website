b5c6d863a005dbbbb55b75c1cc2f232ad00d83f4
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-02-02 19:26:20 +0900

    modified

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
   return (
     <div
     style={{backgroundImage: "url(/mountains.jpg)"}}
-     className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
+    className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
         {Projects.map((project, index) => (
           <ProjectCard
