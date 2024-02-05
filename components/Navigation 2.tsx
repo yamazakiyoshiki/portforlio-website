ca8fe7174ad47aa654a1d46a93fa8b49cda52797
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-02-05 18:46:21 +0900

    secound process

---
+++ components/Navigation 2.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react'
+
+
+const Navigation = () => {
+  const [isRouting, setIsRouting] = useState(false)
+  return (
+    <div>
+      Navigation
+    </div>
+  )
+}
+
+export default Navigation
